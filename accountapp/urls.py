94465f79a31a7c48cbe452348acbf60a6356a95a
DESKTOP-SEOUL\hofather <hofather@gmail.com>
2021-02-11 21:33:13 +0900

    django course 21 CreateView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp" #이렇게 써놓으면 나중에 path 지정할때 좋다.
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create'), #class 형의 view인 경우에는 as_view()메소드 사용.
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from accountapp.models import HelloWorld
 
 
@@ -18,3 +22,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list':hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') #class일 때는 lazy를 쓴다. def일 경우 reverse 사용.
+    template_name = 'accountapp/create.html'
