03f70bbc4f3dd3256bfd4141e6840a192f3f196b
DESKTOP-SEOUL\hofather <hofather@gmail.com>
2021-02-11 21:55:05 +0900

    django course 22 Login&OutView

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -6,5 +7,8 @@ app_name = "accountapp" #이렇게 써놓으면 나중에 path 지정할때 좋�
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     path('create/', AccountCreateView.as_view(), name='create'), #class 형의 view인 경우에는 as_view()메소드 사용.
 ]
