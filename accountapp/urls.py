a0435d1b7100a512fa2f814528153e9b0c2697ec
DESKTOP-SEOUL\hofather <hofather@gmail.com>
2021-02-14 02:32:13 +0900

    Django Course26 Delete&CreateForm

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp" #이렇게 써놓으면 나중에 path 지정할때 좋다.
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'), #class 형의 view인 경우에는 as_view()메소드 사용.
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
